f1281b6fd37b422852eeca2c301b074873fb1f18
Ścibor Budkowski <scibor.budkowski@gmail.com>
2020-09-14 10:59:36 +0200

    filtering messages started

--- RNDM/Controller/MainVC.swift
+++ RNDM/Controller/MainVC.swift
@@ -18,6 +18,7 @@ class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private var thoughts = [Thought]()
     private var thoughtsCollectionRef: CollectionReference!
     private var thoughtsListener: ListenerRegistration!
+    private var selectedCategory = ThoughtCategory.funny.rawValue
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,11 @@ class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        thoughtsListener = thoughtsCollectionRef.addSnapshotListener { (snapshot, error) in
+        setListener()
+    }
+    
+    func setListener() {
+        thoughtsListener = thoughtsCollectionRef.whereField(CATEGORY, isEqualTo: selectedCategory).addSnapshotListener { (snapshot, error) in
             if let error = error {
                 print("Error fetching docs: \(error)")
             } else {
@@ -50,27 +55,20 @@ class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
                     self.thoughts.append(newThought)
                 }
                 self.tableView.reloadData()
+            }
         }
-        
-//        thoughtsCollectionRef.getDocuments { (snapshot, error) in
-//            if let error = error {
-//                print("Error fetching docs: \(error)")
-//            } else {
-//                guard let snap = snapshot else { return }
-//                for document in snap.documents {
-//                    let data = document.data()
-//                    let username = data[USERNAME] as? String ?? "Anonymous"
-//                    let timestamp = data[TIMESTAMP] as? Date ?? Date()
-//                    let thoughtTxt = data[THOUGHT_TXT] as? String ?? ""
-//                    let numLikes = data[NUM_LIKES] as? Int ?? 0
-//                    let numComments = data[NUM_COMMENTS] as? Int ?? 0
-//                    let documentId = document.documentID
-//
-//                    let newThought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
-//                    self.thoughts.append(newThought)
-//                }
-//                self.tableView.reloadData()
-//            }
+    }
+    
+    @IBAction func categoryCHANGED(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            selectedCategory = ThoughtCategory.funny.rawValue
+        case 1:
+            selectedCategory = ThoughtCategory.serious.rawValue
+        case 2:
+            selectedCategory = ThoughtCategory.crazy.rawValue
+        default:
+            selectedCategory = ThoughtCategory.popular.rawValue
         }
     }
     
